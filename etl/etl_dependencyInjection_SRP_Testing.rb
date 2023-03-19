f6e74d9d28bd9dc1e69e17c5d5c02b23c009f4ab
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2023-03-19 18:29:23 -0400

    ETL Demo: Add input and output files

--- etl/etl_dependencyInjection_SRP_Testing.rb
+++ etl/etl_dependencyInjection_SRP_Testing.rb
@@ -6,4 +6,23 @@ class Speak
 
 end
 
-puts Speak.new.add_yo("17")
+# puts Speak.new.add_yo("17")
+
+
+class Action
+
+  def extract_transform_load
+    file = File.open("input.txt")
+    data = file.read
+    file.close
+
+    # puts data
+
+
+    File.open("output.txt", "w") { |file| file.write(data) }
+    return
+  end
+
+end
+
+puts Action.new.extract_transform_load
