fce73e8a8710387305ea3c3123ce60726475094e
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2023-03-19 18:53:08 -0400

    ELT Demo: Refactor transformation - BRANCH HERE

--- etl/etl_dependencyInjection_SRP_Testing.rb
+++ etl/etl_dependencyInjection_SRP_Testing.rb
@@ -8,25 +8,20 @@ end
 
 # puts Speak.new.add_yo("17")
 
+# don't forget to manipultate input file with one `>`
 
 class Action
 
-  def extract_transform_load
+  def run
     file = File.open("input.txt")
     data = file.read
     file.close
 
-    # puts data
-
-    is_odd = data.to_i.odd?
-    meets_threshold = data.to_i > 100
-
     File.open("output.txt", "w") { |file| file.write(
-      "Odd?: #{is_odd}; Meets threshold?: #{meets_threshold}"
+      "Odd?: #{data.to_i.odd?}; Meets threshold?: #{data.to_i > 100}"
     ) }
-    return
   end
 
 end
 
-puts Action.new.extract_transform_load
+Action.new.run
