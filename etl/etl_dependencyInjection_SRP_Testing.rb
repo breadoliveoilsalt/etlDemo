515958138643ffb2e55bda7b09037ff130a761b7
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2023-03-20 17:31:09 -0400

    WIP: Extract File methods into wrappers

--- etl/etl_dependencyInjection_SRP_Testing.rb
+++ etl/etl_dependencyInjection_SRP_Testing.rb
@@ -10,18 +10,50 @@ end
 
 # don't forget to manipultate input file with one `>`
 
-class Action
 
-  def run
+ 
+class Input 
+
+  def getInput
     file = File.open("input.txt")
     data = file.read
     file.close
+    data
+  end
 
-    File.open("output.txt", "w") { |file| file.write(
-      "Odd?: #{data.to_i.odd?}; Meets threshold?: #{data.to_i > 100}"
-    ) }
+end
+
+class Output
+
+  def postOutput(dataToPost)
+    File.open("output.txt", "w") { |file| file.write(dataToPost) }
+  end
+
+
+end
+
+class Action
+
+  def run
+    data = Input.new.getInput
+
+    transformedData = "Odd?: #{data.to_i.odd?}; Meets threshold?: #{data.to_i > 100}"
+
+    Output.new.postOutput(transformedData)
   end
 
 end
 
 Action.new.run
+
+
+
+# FOR UNIT TESTING
+# 1. It's ok to think
+# 2. If completely lost, just right the test description
+# 3. Wrap what you don't own; what is black box to you.
+  # SRP
+# 4. With whatever's left -- starting point is to organize your code into pure functions/methods as much as possible. Takes arguement(s), has return value -- and not matter what, if you pass the same args in, you always get the same results, the same value back.
+  # Easier testing
+  # SRP
+  # Data structure - ok to be creative, so long the unit tests don't get ugly
